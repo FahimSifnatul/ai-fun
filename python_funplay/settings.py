7b21ab36d2c4249f8c8cf467a5a2893ccf91f8ab
FahimSifnatul <44759537+FahimSifnatul@users.noreply.github.com>
2021-08-15 12:24:34 +0600

    Set DEBUG to False

--- python_funplay/settings.py
+++ python_funplay/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6+)4_irrxj&vyiv%z6c%oan^(_zvq0zho6ed#rr^ro(h2h6v*9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['python-funplay.herokuapp.com', '127.0.0.1', '.localhost', '[::1]']
 
